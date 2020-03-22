10aecea6b3d893ccb41b23588aee1d1810e77640
Angela Vawser <amvawser@gmail.com>
2020-03-21 19:53:36 -0700

    app container hooks up the fetch function to the form and returns result

---
+++ src/containers/App.js
@@ -0,0 +1,44 @@
+import React,{useState} from 'react';
+import './App.css';
+import Form from '../components/Form';
+import Weather from '../components/Weather';
+
+function App() {
+  const [weather,setWeather] = useState([])
+  const API_KEY = `9e21eecdf587c32af6325f3e4e1aec1c`
+
+  async function fetchData(e) {
+    const city = e.target.elements.city.value
+    const country = e.target.elements.country.value
+    e.preventDefault()
+    const apiData = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=${API_KEY}`)
+    .then( res => res.json())
+    .then(data => data)
+
+    setWeather({
+      data: apiData,
+      city: apiData.city,
+      country: apiData.sys.country,
+      description: apiData.weather[0].description,
+      temperature: apiData.main.temp,
+      error:""
+    })
+  }
+
+  return (
+    <div className="App">
+      <h3>WEATHER APP</h3>
+      <Form getWeather={fetchData} />
+      <Weather
+      city={weather.city}
+      country={weather.country}
+      description={weather.description}
+      temperature={weather.temperature}
+      error={weather.error}
+      />
+      {console.log(weather.data)}
+    </div>
+  );
+}
+
+export default App;
